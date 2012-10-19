5058f4cc3e9454f8763a77ffafadec0d90b37a90
Marc Urben <aegnor@mittelerde.ch>
2012-10-20 01:59:46 +0200

    gabi fun update

--- gabifun.py
+++ gabifun.py
@@ -6,4 +6,12 @@ class GabiFun(BotBase):
     @botcmd
     def fetzig (self, mess, args='Aggravate'):
         """Lets fetz"""
-        return 'Fetzig? Hab ich fetzig gehoert? ' + args  + '  ist fetzig!';
+        return 'Fetzig? Hab ich fetzig gehoert? ' + args  + ' ist fetzig!';
+
+    @botcmd
+    def imba (self, mess, args):
+        """Etwas ist IMBA"""
+		if len(args) > 0:
+		    return args  + ' ist IMBA!';
+		else:
+			return 'Ich bin IMBA!';
