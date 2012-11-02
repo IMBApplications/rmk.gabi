6e5655bd311d06c9df1fa2d809b10edc266767a5
skeeve55 <skeeve55@gmx.de>
2012-11-02 11:41:00 +0100

    Update botbase.py

--- botbase.py
+++ botbase.py
@@ -208,7 +208,7 @@ class BotBase(object):
 
 	def send(self, user, text, in_reply_to=None, message_type='chat'):
 		"""Sends a simple message to the specified user."""
-		mess = self.build_message(text)
+		mess = '|c:7|' + self.build_message(text)
 		mess.setTo(user)
 
 		if in_reply_to:
