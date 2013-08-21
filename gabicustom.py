281cea547da6e5cf8b1497453d9affff478019fd
Marc Urben <aegnor@mittelerde.ch>
2013-08-21 14:45:31 +0200

    unicode hell

--- gabicustom.py
+++ gabicustom.py
@@ -83,7 +83,7 @@ class GabiCustom(BotBase):
             handler.close()
 
     def on_came_online(self, jid):
-        jidSplit = self.list_unicode_cleanup(jid.split('/'))
+        jidSplit = self.list_unicode_cleanup(jid.__repr__().split('/'))
         # jidSplit = '{0}'.format(self.convert_from_unicode(jid)).partition('/')
         room = jidSplit[0]
         user = jidSplit[2]
