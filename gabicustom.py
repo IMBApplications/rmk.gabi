f848512929a4cb0181f6487c05717d893d0ef6bb
Marc Urben <aegnor@mittelerde.ch>
2013-10-03 09:46:53 +0200

    working on count

--- gabicustom.py
+++ gabicustom.py
@@ -349,6 +349,8 @@ class GabiCustom(BotBase):
                 count += 1
                 target_time = datetime.datetime.fromtimestamp(timestamp, pytz.timezone(self.timezone))
                 ret_message.append('%s\t%s\t"%s" von "%s"' % (count, target_time.strftime("%a, %d %b %Y %H:%M:%S"), message, user))
+            if count == 0:
+                ret_message = ['Es sind keine Zaehler gesetzt.']
         elif args[0].lower() == "del":
             ret_message = ["Unbekannter index. Bitte gib einen zulaessigen index an (count list)."]
             try:
