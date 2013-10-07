9fb13facea3500219dbb7953028af811e2177408
Marc Urben <aegnor@mittelerde.ch>
2013-10-07 10:54:01 +0200

    working on count

--- gabicustom.py
+++ gabicustom.py
@@ -461,10 +461,10 @@ class GabiCustom(BotBase):
 
                     if futureTimeDiff < (dayInSecs * 2):
                         self.countTopic.append((timestamp, "Morgen: %s" % (message)))
-                    elif futureTimeDiff < (dayInSecs * 6):
-                        self.countTopic.append((timestamp, "In %s Tagen: %s" % (int(futureTimeDiff / dayInSecs) + 1, message)))
                     elif futureTimeDiff < (dayInSecs * 7):
                         self.countTopic.append((timestamp, "In 1 Woche: %s" % (message)))
+                    elif futureTimeDiff < (dayInSecs * 13):
+                        self.countTopic.append((timestamp, "In %s Tagen: %s" % (int(futureTimeDiff / dayInSecs) + 1, message)))
                     elif (futureTimeDiff % (dayInSecs * 7)) == 0 and (futureTimeDiff % (dayInSecs * 7)) < 5:
                         self.countTopic.append((timestamp, "In %s Wochen: %s" % (int(futureTimeDiff / (dayInSecs * 7)), message)))
 
