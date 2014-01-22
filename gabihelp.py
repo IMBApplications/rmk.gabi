af315836a59a954460490b9d3948eca1fc91fd68
Marc Urben <aegnor@mittelerde.ch>
2014-01-22 15:07:33 +0100

    multiline?

--- gabihelp.py
+++ gabihelp.py
@@ -26,11 +26,11 @@ class GabiHelp(BotBase):
                 description = 'Available commands:'
 
             usage = sorted(['%s:\t%s' % (name, (command.__doc__ or \
-                                '(undocumented)').strip().split('\n', 1)[0]
+                                '(undocumented)').strip().split('\n', 1)[0])
                     for (name, command) in self.commands.iteritems() \
                         if name != 'help' \
                         and not command._jabberbot_command_hidden
-            ]))
+            ])
             #usage = '\n\n' + '\n\n'.join(filter(None, [usage, self.MSG_HELP_TAIL]))
             usage = filter(None, [usage, self.MSG_HELP_TAIL])
         else:
