b14d83f7f74b74ee047136f21bd53170a05107b9
Marc Urben <aegnor@mittelerde.ch>
2014-01-27 08:49:58 +0100

    html messages

--- botbase.py
+++ botbase.py
@@ -292,13 +292,13 @@ class BotBase(object):
                 newText = ""
                 if allowHtml:
                     for line in text:
-                        newText += line + '<br />'
+                        newText += cgi.escape(unicode(str(line), "utf-8")).encode('ascii') + '<br />'
                 else:
                     for line in text:
                         newText += cgi.escape(unicode(str(line), "utf-8")).encode('ascii', 'xmlcharrefreplace') + '<br />'
             else:
                 if allowHtml:
-                    newText = text
+                    newText = cgi.escape(unicode(str(text), "utf-8")).encode('ascii')
                 else:
                     newText = cgi.escape(unicode(str(text), "utf-8")).encode('ascii', 'xmlcharrefreplace')
             # Create body w stripped tags for reciptiens w/o xhtml-abilities
