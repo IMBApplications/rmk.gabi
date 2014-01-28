b695ff539850e70bc09fc492bac1b6190f86a432
Marc Urben <aegnor@mittelerde.ch>
2014-01-28 10:24:43 +0100

    xhtml

--- gabilog.py
+++ gabilog.py
@@ -28,7 +28,7 @@ class GabiLog(BotBase):
             
             username = cgi.escape(self.encode_message(username)).encode("ascii", "xmlcharrefreplace")
             url = cgi.escape(self.encode_message(url)).encode("ascii", "xmlcharrefreplace")
-            title = self.encode_message(title).encode("ascii", "xmlcharrefreplace")
+            title = cgi.escape(self.encode_message(title)).encode("ascii", "xmlcharrefreplace")
             print title
             try:
                 #newTitle = cgi.escape(unicode(str(title), "utf-8"))
