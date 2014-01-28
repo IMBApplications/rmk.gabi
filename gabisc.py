f0c7d88a57e432ce4c06eb0357bd0901e39b1692
Marc Urben <aegnor@mittelerde.ch>
2014-01-28 14:11:46 +0100

    scnews

--- gabisc.py
+++ gabisc.py
@@ -75,12 +75,12 @@ class GabiStarCitizen(BotBase):
 
         count = 0
         feedData.append(_("<a href='{0}' alt='{1}'>{2}</a>").format(feedLink, feedDescription, feedTitle))
-        for entry in reversed(rss.entries):
+        for entry in rss.entries:
             if count <= 9:
                 count += 1
             else:
                 break
-                
+
             entryLink = cgi.escape(self.encode_message(entry.link)).encode("ascii", "xmlcharrefreplace")
             entryDescription = cgi.escape(self.encode_message(entry.description)).encode("ascii", "xmlcharrefreplace")
             entryPublished = cgi.escape(self.encode_message(entry.published)).encode("ascii", "xmlcharrefreplace")
