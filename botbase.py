e460804ab4fb7a4ed58a6012fa83317d5a4e8ad4
Marc Urben <aegnor@mittelerde.ch>
2014-01-28 09:15:39 +0100

    xht,l

--- botbase.py
+++ botbase.py
@@ -291,7 +291,7 @@ class BotBase(object):
             self.log.warning('Content is no string: %s' % (content))
             unicode_content = content
 
-        return unicode_content.encode('ascii', 'replace')
+        return unicode_content #.encode('ascii', 'replace')
 
 
     def build_message(self, text):
