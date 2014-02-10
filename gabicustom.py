cf69cb3a1b00fbe14af6530a3dbb24c4bdec04d5
Marc Urben <aegnor@mittelerde.ch>
2014-02-10 15:21:46 +0100

    utf8 hell

--- gabicustom.py
+++ gabicustom.py
@@ -195,7 +195,12 @@ class GabiCustom(BotBase):
                     newUrl = False
 
             if htmlTitle and newUrl:
-                self.send_simple_reply(mess, _("URL title from {0}: <a href='{2}' target='_blank'>{1}</a>").format(username, htmlTitle.decode('latin1').encode("ascii", "ignore"), url))
+                test = htmlTitle.encode('utf-8')
+                try:
+                    htmlTitle = htmlTitle.decode('latin1').encode("ascii", "ignore")
+                except:
+                    pass
+                self.send_simple_reply(mess, _("URL title from {0}: <a href='{2}' target='_blank'>{1}</a>").format(username, htmlTitle, url))
             else:
                 htmlTitle = ""
 
