5363db0ec9b229420ba6c2f175cae04fb9ffbe9c
Marc Urben <aegnor@mittelerde.ch>
2014-09-01 16:01:38 +0200

    unicode topic

--- botbase.py
+++ botbase.py
@@ -642,7 +642,7 @@ class BotBase(object):
                 break
             newTopic.append(topic)
 
-        newTmpTopic = ' | '.join(newTopic)
+        newTmpTopic = ' | '.join(newTopic).decode('utf-8')
         if newTmpTopic != self.currentTopic:
             self.currentTopic = newTmpTopic
 
