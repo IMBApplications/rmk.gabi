e3b9a89d5138c22edead68207e743623062257f8
Marc Urben <aegnor@mittelerde.ch>
2014-09-01 15:52:48 +0200

    topic utf8 hate

--- botbase.py
+++ botbase.py
@@ -642,7 +642,7 @@ class BotBase(object):
                 break
             newTopic.append(topic)
 
-        newTmpTopic = ' | '.join(newTopic)
+        newTmpTopic = ' | '.join(newTopic.encode('utf8'))
         if newTmpTopic != self.currentTopic:
             self.currentTopic = newTmpTopic
 
