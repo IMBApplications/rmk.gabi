852620ba8ce2268e809c5f7d5022a726095ed8e9
Marc Urben <aegnor@mittelerde.ch>
2014-09-01 15:56:25 +0200

    unicode schrott

--- botbase.py
+++ botbase.py
@@ -642,7 +642,7 @@ class BotBase(object):
                 break
             newTopic.append(topic)
 
-        newTmpTopic = ' | '.join(newTopic).encode('utf8')
+        newTmpTopic = ' | '.join(newTopic)
         if newTmpTopic != self.currentTopic:
             self.currentTopic = newTmpTopic
 
